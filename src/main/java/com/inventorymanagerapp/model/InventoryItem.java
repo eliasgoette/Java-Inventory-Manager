e208eda26c59e61738c3d1c667acfd935f98cb57
Elias G <83478605+eliasgoette@users.noreply.github.com>
2023-12-29 16:07:52 +0000

    Added model for InventoryItem

---
+++ src/main/java/com/inventorymanagerapp/model/InventoryItem.java
@@ -0,0 +1,31 @@
+package main.java.com.inventorymanagerapp.model;
+
+public class InventoryItem {
+    private String id;
+    private String name;
+    private int quantity;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
